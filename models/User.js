6ae2c1635ab804796b01e1c718acc699f1fbff8c
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-09-26 16:00:14 -0500

    User controller and model set up

--- models/User.js
+++ models/User.js
@@ -1,1 +1,24 @@
 const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/],
+  },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
+
+// create the user model using the UserSchema
+const User = model("User", UserSchema);
+
+// export the User model
+module.exports = User;
