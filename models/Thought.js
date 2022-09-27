ca55fcff30c545724383e8db7833574beed12d20
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-09-27 16:07:58 -0500

    Thought model

---
+++ models/Thought.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
